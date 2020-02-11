66025158f09d8186d7fbbb97d9c445f14012aed6
Никита Красавин <6nike6@bk.ru>
2020-02-11 15:51:37 +0300

    remove appcenter build script

--- appcenter-post-build.sh
+++
@@ -1,7 +0,0 @@
-echo "Running tests..."
-xcodebuild \
-    -workspace NewsApp.xcworkspace \
-    -scheme NewsApp \
-    -sdk iphonesimulator \
-    -destination 'platform=iOS Simulator,name=iPhone 11,OS=13.3' \
-    test | xcpretty --test --color
